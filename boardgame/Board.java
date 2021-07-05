7fa8ddfba0e8f86022cd1b4efff513a438581fb2
gabrizl <gsilva.pb@gmail.com>
2021-07-05 11:09:21 -0300

    BoardException and defensive programming

--- boardgame/Board.java
+++ boardgame/Board.java
@@ -1,6 +1,6 @@
 package boardgame;
 
-
+import jdk.tools.jlink.resources.plugins;
 
 public class Board {
   private int rows;
@@ -9,6 +9,9 @@ public class Board {
 
 
   public Board(int rows, int columns) {
+    if (rows <1 || columns < 1){
+      throw new BoardException("Error creating board: there must be at least 1 row and 1 column.");  
+    }
     this.rows = rows;
     this.columns = columns;
     pieces = new Piece[rows][columns];
@@ -22,9 +25,6 @@ public int getRows() {
 }
 
 
-public void setRows(int rows) {
-    this.rows = rows;
-}
 
 
 public int getColumns() {
@@ -32,22 +32,40 @@ public int getColumns() {
 }
 
 
-public void setColumns(int columns) {
-    this.columns = columns;
-}
 
 public Piece piece(int row, int column){
+    if(!positionExists(row, column)){
+        throw new BoardException("Position not on the board");
+    }
     return pieces[row][column];
 
 }
 public Piece piece(Position position){
+    if(!positionExists(position)){
+        throw new BoardException("Position not on the board");
+    }
+    
     return pieces[position.getRow()][position.getColumn()];
 }
 
 public void placePiece(Piece piece, Position position){
+    if(thereIsAPiece(position)){
+        throw new BoardException("There is already a piece on position"+ position);
+    }
     pieces[position.getRow()][position.getColumn()] = piece;
     piece.position = position;
-    
-}   
 
+} 
+private boolean positionExists(int row, int column){
+   return row >= 0 && row < rows && column >=0 && column < columns;
+}
+public boolean positionExists(Position position){
+    return positionExists(position.getRow(), position.getColumn());
+}  
+public boolean thereIsAPiece(Position position){
+    if(!positionExists(position)){
+        throw new BoardException("Position not on the board");
+    }
+    return piece(position) != null;
+}
 }
--- chess/ChessMatch.java
+++ chess/ChessMatch.java
@@ -22,8 +22,9 @@ public class ChessMatch {
         }
         return mat;
     }
-    private void initalSetup(){
-        board.placePiece(new Rook(board, Color.WHITE), new Position(2,1));
+
+    private void initalSetup() {
+        board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
         board.placePiece(new King(board, Color.BLACK), new Position(0, 4));
         board.placePiece(new King(board, Color.WHITE), new Position(7, 4));
 
